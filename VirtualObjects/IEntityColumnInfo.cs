b565eec071acd81d60aa5c1683bbb262d82f4b03
AlienEngineer <alien.software.engineer@gmail.com>
2014-02-15 13:01:30 +0000

    Added some comments

--- VirtualObjects/IEntityColumnInfo.cs
+++ VirtualObjects/IEntityColumnInfo.cs
@@ -3,35 +3,123 @@ using System.Reflection;
 
 namespace VirtualObjects
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IEntityColumnInfo
     {
+        /// <summary>
+        /// Gets the name of the column.
+        /// </summary>
+        /// <value>
+        /// The name of the column.
+        /// </value>
         String ColumnName { get; }
 
+        /// <summary>
+        /// Gets a value indicating whether [is key].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [is key]; otherwise, <c>false</c>.
+        /// </value>
         Boolean IsKey { get; }
 
+        /// <summary>
+        /// Gets a value indicating whether [is identity].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [is identity]; otherwise, <c>false</c>.
+        /// </value>
         Boolean IsIdentity { get; }
 
+        /// <summary>
+        /// Gets the property.
+        /// </summary>
+        /// <value>
+        /// The property.
+        /// </value>
         PropertyInfo Property { get; }
 
+        /// <summary>
+        /// Gets the entity information.
+        /// </summary>
+        /// <value>
+        /// The entity information.
+        /// </value>
         IEntityInfo EntityInfo { get;  }
-        
+
+        /// <summary>
+        /// Gets or sets the foreign key.
+        /// </summary>
+        /// <value>
+        /// The foreign key.
+        /// </value>
         IEntityColumnInfo ForeignKey { get; set; }
 
+        /// <summary>
+        /// Gets the bind name or the name.
+        /// </summary>
+        /// <value>
+        /// The name of the bind or.
+        /// </value>
         string BindOrName { get; }
 
+        /// <summary>
+        /// Gets the value getter.
+        /// </summary>
+        /// <value>
+        /// The value getter.
+        /// </value>
         Func<Object, Object> ValueGetter { get; }
 
+        /// <summary>
+        /// Gets the value setter.
+        /// </summary>
+        /// <value>
+        /// The value setter.
+        /// </value>
         Action<Object, Object> ValueSetter { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether [is version control].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [is version control]; otherwise, <c>false</c>.
+        /// </value>
         bool IsVersionControl { get; }
 
+        /// <summary>
+        /// Sets the value.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <param name="value">The value.</param>
         void SetValue(Object entity, Object value);
 
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
         object GetValue(Object entity);
 
+        /// <summary>
+        /// Gets the field final value.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
         object GetFieldFinalValue(object entity);
 
+        /// <summary>
+        /// Sets the field final value.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <param name="value">The value.</param>
         void SetFieldFinalValue(object entity, object value);
 
+        /// <summary>
+        /// Gets the last bind.
+        /// </summary>
+        /// <returns></returns>
         IEntityColumnInfo GetLastBind();
         
         
