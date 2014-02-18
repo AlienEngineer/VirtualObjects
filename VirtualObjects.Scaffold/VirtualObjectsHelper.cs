f4d4019c5842d917580073427967bf43b5e346de
AlienEngineer <Alien.Software.Engineer@gmail.com>
2014-02-18 18:11:12 +0000

    using INFORMATION_SCHEMA
    Verify this for other versions of SQL

--- VirtualObjects.Scaffold/VirtualObjectsHelper.cs
+++ VirtualObjects.Scaffold/VirtualObjectsHelper.cs
@@ -9,28 +9,28 @@ namespace VirtualObjects.Scaffold
     public static class VirtualObjectsHelper
     {
 
-        [Table(TableName = "sys.objects")]
+        [Table(TableName = "INFORMATION_SCHEMA.TABLES")]
         public class MetaTable
         {
-            [Key(FieldName = "Object_Id")]
-            public int Id { get; set; }
-
+            [Key(FieldName = "TABLE_NAME")]
             public String Name { get; set; }
 
+            [Column(FieldName = "TABLE_TYPE")]
             public String Type { get; set; }
 
             public IEnumerable<MetaColumn> Columns { get; set; }
         }
 
+        [Table(TableName = "INFORMATION_SCHEMA.COLUMNS")]
         public class MetaColumn
         {
-            [Association(FieldName = "Object_Id", OtherKey = "Id")]
+            [Key(FieldName = "TABLE_NAME")]
+            [Association(FieldName = "TABLE_NAME", OtherKey = "Name")]
             public virtual MetaTable Table { get; set; }
 
-            [Key(FieldName = "Column_Id")]
-            public int Id { get; set; }
-
+            [Key(FieldName = "COLUMN_NAME")]
             public String Name { get; set; }
+
             public Boolean IsKey { get; set; }
             
             [Column(FieldName = "Is_Identity")]
