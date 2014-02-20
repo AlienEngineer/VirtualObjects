7021b8e669170cabdf71f56ef5cb3ef180a699b9
AlienEngineer <Alien.Software.Engineer@gmail.com>
2014-02-20 17:47:22 +0000

    Initial analisys to connection Schema

--- VirtualObjects.Tests/Scaffold/VirtualObjectsHelperTests.cs
+++ VirtualObjects.Tests/Scaffold/VirtualObjectsHelperTests.cs
@@ -6,8 +6,9 @@ using FluentAssertions;
 namespace VirtualObjects.Tests.Scaffold
 {
     using NUnit.Framework;
-    using Microsoft.SqlServer.Management.Smo;
     using System.Data;
+    using System.Data.SqlClient;
+    using System.Data.Common;
 
     [TestFixture, Category("VirtualObjectsHelper")]
     public class VirtualObjectsHelperTests : UtilityBelt
@@ -16,41 +17,72 @@ namespace VirtualObjects.Tests.Scaffold
         [Test]
         public void Helper_Can_Produce_TablesInformation()
         {
-            
-            Server server = new Server(@".\development");
-            Database database = new Database(server, "northwind");
-            database.Refresh();
 
-            foreach ( Table table in database.Tables )
+            var connection = Connection as DbConnection;
+
+            connection.Open();
+
+            var table = connection.GetSchema(SqlClientMetaDataCollectionNames.Tables, new String[] { null, null, null, "BASE TABLE" });
+
+
+            // Display the contents of the table.
+            foreach ( System.Data.DataRow row in table.Rows )
             {
-                foreach ( Column column in table.Columns )
+                Console.WriteLine("{0}", row["Table_Name"]);
+
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = String.Format("Select * From [{0}]", row["Table_Name"]);
+                var reader = cmd.ExecuteReader(CommandBehavior.SchemaOnly);
+
+                var columnsInfo = reader.GetSchemaTable();
+                
+                foreach ( DataRow columnInfo in columnsInfo.Rows )
                 {
-                    column.Name.Should().NotBeEmpty();
-                    if ( column.IsForeignKey )
-                    {
-                        foreach ( ForeignKey foreignKeys in table.ForeignKeys )
-                        {
-                            foreach ( ForeignKeyColumn foreignColumn in foreignKeys.Columns )
-                            {
-                                if ( foreignColumn.Parent.Parent.Name == table.Name && foreignColumn.Name == column.Name )
-                                {
-                                    Console.Write(String.Format("[Association(FieldName=\"{0}\", OtherKey = \"{1}\")]", column.Name, foreignColumn.ReferencedColumn));
-                                    Console.WriteLine("");
+                    Console.WriteLine(" Name: {0}", columnInfo["ColumnName"]);
+                    Console.WriteLine(" IsIdentity: {0}", columnInfo["IsIdentity"]);
+                    Console.WriteLine(" IsKey: {0}", columnInfo["IsKey"]);
+                    Console.WriteLine(" DataTypeName: {0}", columnInfo["DataTypeName"]);
+                }
+                
+                reader.Close();
 
-                                    Console.Write("public ");
-                                    Console.Write(" virtual ");
-                                    Console.Write(foreignColumn.Parent.ReferencedTable.Replace(" ", ""));
-                                    Console.Write(" ");
-                                    Console.Write(foreignColumn.Parent.ReferencedTable.Replace(" ", ""));
-                                }
-                            }
-                        }
+                foreach ( DataRelation relation in columnsInfo.ChildRelations )
+                {
+                    var fk = relation.ChildKeyConstraint;
 
-                    }
+                    Console.WriteLine(" RelatedTable {0}; RelatedColums {1}", fk.RelatedTable, fk.RelatedColumns);
                 }
 
+                Console.WriteLine("============================");
             }
 
+        }
+
+        private static Boolean IsPrimaryKey(DataTable table)
+        {
+            foreach ( DataRow r in table.Rows )
+            {
+                foreach ( DataColumn c in table.Columns )
+                {
+                    if ( c.ColumnName == "KeyType" )
+                    {
+                        return Convert.ToInt32(r[c]) == 56;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static void DisplayData(System.Data.DataTable table)
+        {
+            foreach ( System.Data.DataRow row in table.Rows )
+            {
+                foreach ( System.Data.DataColumn col in table.Columns )
+                {
+                    Console.WriteLine("{0} = {1}", col.ColumnName, row[col]);
+                }
+                Console.WriteLine("============================ {0}", table.Columns.Count);
+            }
         }
 
     }
