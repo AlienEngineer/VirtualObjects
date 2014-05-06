0a21411067b67f7950ba0e9dea2aad3375f2b5d2
AlienEngineer <Aimirim.Software@gmail.com>
2014-05-06 14:43:41 +0100

    Entity projection

--- VirtualObjects.Tests/Queries/SqlTranslationTests.cs
+++ VirtualObjects.Tests/Queries/SqlTranslationTests.cs
@@ -899,6 +899,20 @@ namespace VirtualObjects.Tests.Queries
             );
         }
 
+
+        [Test, Repeat(Repeat)]
+        public void SqlTranslation_Simple_EntityProjection_Join()
+        {
+            var query = from o in Query<Orders>()
+                        join od in Query<OrderDetails>() on o equals od.Order
+                        select o;
+
+            Assert.That(
+                Translate(query),
+                Is.EqualTo("Select [T0].[OrderId], [T0].[CustomerId], [T0].[EmployeeId], [T0].[OrderDate], [T0].[RequiredDate], [T0].[ShippedDate], [T0].[ShipVia], [T0].[Freight], [T0].[ShipName], [T0].[ShipAddress], [T0].[ShipCity], [T0].[ShipRegion], [T0].[ShipPostalCode], [T0].[ShipCountry] From [Orders] [T0] Inner Join [Order Details] [T1] On ([T0].[OrderId] = [T1].[OrderId])")
+            );
+        }
+
         [Test, Repeat(Repeat)]
         public void SqlTranslation_Simple_NonKey_Join()
         {
